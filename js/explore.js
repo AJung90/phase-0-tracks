92f7af80d6fea25cdd416b1a3d5d05294255d4c0
Andrew Jung <ajung90@comcast.net>
2016-05-24 01:23:56 -0500

    Start to add pseudocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Declare a function that takes a string as a parameter and reverses the string 
+
+var str = prompt("Please enter a word or phrase", "<input goes here>");
+
+for (var str.reverse) {
+	console.log()
+}
+
