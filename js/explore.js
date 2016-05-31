84d04a287ccfd62b0b835d4a51a9a20e3acef95b
Andrew Jung <ajung90@comcast.net>
2016-05-31 03:40:02 -0500

    Add function to reverse string and driver code

--- js/explore.js
+++ js/explore.js
@@ -1,8 +1,20 @@
 // Declare a function that takes a string as a parameter and reverses the string 
+// PSEUDOCODE
+// -----------
+// Create a function that takes a string and splits it into an array
+// Take all the items in the array and reverse their order within the array
+// Join the items back together into a string
+// Within the function, return the new string
 
-var str = prompt("Please enter a word or phrase", "<input goes here>");
 
-for (var str.reverse) {
-	console.log()
+function reverse(string) {
+	return string.split("").reverse().join("");
 }
 
+// DRIVER CODE
+// ------------
+var wordReverse = reverse("hello")
+// Print the variable if the condition is true
+if (1 == 1) {
+  console.log(wordReverse);
+}
