6687c9868a2b65a51b58b794a3ed3699ff5bf049
Andrew Jung <ajung90@comcast.net>
2016-06-06 03:19:11 -0500

    Add create player table

--- databases/8.5_Ruby_SQL/recruiting_form.rb
+++ databases/8.5_Ruby_SQL/recruiting_form.rb
@@ -7,3 +7,19 @@ require 'sqlite3'
 # create SQLite3 database
 db = SQLite3::Database.new("player_profiles.db")
 db.results_as_hash = true
+
+# create string delimiter for players
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS players(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT,
+		hometown VARCHAR(255),
+		college1 VARCHAR(255),
+		college2 VARCHAR(255),
+		college3 VARCHAR(255)
+	)
+SQL
+
+# create a players table if it doesn't already exist
+db.execute(create_table_cmd)
