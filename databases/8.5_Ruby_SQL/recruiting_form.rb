0117bf59ce13dd41fb406ccea21b1e70795c2892
Andrew Jung <ajung90@comcast.net>
2016-06-06 04:25:56 -0500

    Add user interface

--- databases/8.5_Ruby_SQL/recruiting_form.rb
+++ databases/8.5_Ruby_SQL/recruiting_form.rb
@@ -23,3 +23,38 @@ SQL
 
 # create a players table if it doesn't already exist
 db.execute(create_table_cmd)
+
+# allow for user input / user interface
+grad_year = ""
+until grad_year == "done"
+
+puts "Welcome! Here we will assist you in your college search. Let's start by entering your year of graduation."
+grad_year = gets.chomp
+
+puts "Now let's get to the good stuff. After you have finished answering all questions, please type 'done'."
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What city are you from?"
+hometown = gets.chomp
+
+puts "What is your top choice for college?"
+college1 = gets.chomp
+
+puts "What is your next top choice?"
+college2 = gets.chomp
+
+puts "What is your 3rd choice?"
+college3 = gets.chomp
+
+# Add player input into players table
+db.execute("INSERT INTO players (name, age, hometown, college1, college2, college3) VALUES (?, ?, ?, ?, ?, ?)", [name, age, hometown, college1, college2, college3])
+players = db.execute("SELECT * FROM players")
+p players 
+end
+
+# Will add more to this program this week. Did not have time to make it more intricate.
